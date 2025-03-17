5efce92ea8e4ef4bdf8c5944066cbf12096ca792
noback <Matthias.Noback@deltares.nl>
2025-03-17 09:34:56 +0100

    Assignment 1: greeting and recipient DTs

--- src/hello_world_app/hello_world_lib/m_hello_world.f90
+++ src/hello_world_app/hello_world_lib/m_hello_world.f90
@@ -3,14 +3,38 @@ module m_hello_world
 
    private
 
-   public :: hello_world
+   public :: hello_world, t_greeting, t_recipient
+
+   type :: t_greeting
+      character(len=:), allocatable :: greeting
+   contains
+      procedure :: greet => greeting_greet
+   end type t_greeting
+
+   type :: t_recipient
+      character(len=:), allocatable :: name
+   end type t_recipient
 
 contains
+
+   function greeting_greet(greeting, recipient) result(res)
+      class(t_greeting), intent(in) :: greeting
+      class(t_recipient), intent(in) :: recipient
+      character(len=:), allocatable :: res
+
+      res = greeting%greeting//', '//recipient%name//'!'
+   end function greeting_greet
 
    function hello_world() result(res)
       character(len=:), allocatable :: res
 
-      res = 'Hello, world!'
+      type(t_greeting) :: greeting
+      type(t_recipient) :: recipient
+
+      greeting = t_greeting('Hello')
+      recipient = t_recipient('world')
+
+      res = greeting%greet(recipient)
    end function hello_world
 
 end module m_hello_world
--- test/test_hello_world.f90
+++ test/test_hello_world.f90
@@ -1,6 +1,6 @@
 module test_hello_world
    use testdrive, only: new_unittest, unittest_type, error_type, check, test_failed
-   use m_hello_world, only: hello_world
+   use m_hello_world, only: hello_world, t_greeting, t_recipient
 
    implicit none(type, external)
 
@@ -14,7 +14,8 @@ contains
       type(unittest_type), allocatable, intent(out) :: testsuite(:)
 
       testsuite = [ &
-                  new_unittest("hello_world", test_hello_world_function) &
+                  new_unittest("hello_world", test_hello_world_function), &
+                  new_unittest("test_greeting", test_greeting) &
                   ]
 
    end subroutine collect_hello_world_tests
@@ -24,5 +25,16 @@ contains
 
       call check(error, hello_world(), 'Hello, world!')
    end subroutine test_hello_world_function
+
+   subroutine test_greeting(error)
+      type(error_type), allocatable, intent(out) :: error
+      type(t_greeting) :: greeting
+      type(t_recipient) :: recipient
+
+      greeting = t_greeting('Hey')
+      recipient = t_recipient('mister')
+
+      call check(error, greeting%greet(recipient), 'Hey, mister!')
+   end subroutine test_greeting
 
 end module test_hello_world
