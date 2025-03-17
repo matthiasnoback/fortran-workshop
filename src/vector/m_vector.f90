afda8e5bbb0515d49875febb84dd21a153aea0f4
noback <Matthias.Noback@deltares.nl>
2025-03-17 09:55:11 +0100

    Assignment 2: 3d vector DT

--- src/vector/m_vector.f90
+++ src/vector/m_vector.f90
@@ -1,18 +1,39 @@
 module m_vector
-   use iso_fortran_env, only: real64
+   use iso_fortran_env, only: wp => real64
 
    implicit none(type, external)
 
    private
-   public :: dummy_function
+   public :: t_3d_vector
+
+   type :: t_3d_vector
+      real(kind=wp) :: v1
+      real(kind=wp) :: v2
+      real(kind=wp) :: v3
+   contains
+      procedure :: distance_to => vector_distance_to
+      procedure :: add_to => vector_add_to
+   end type t_3d_vector
 
 contains
 
-   !> Only here to verify that a unit test can load this module and call one of its functions
-   pure function dummy_function() result(res)
-      logical :: res
+   pure function vector_add_to(self, other) result(sum_vector)
+      class(t_3d_vector), intent(in) :: self
+      class(t_3d_vector), intent(in) :: other
+      type(t_3d_vector) :: sum_vector
 
-      res = .true.
-   end function dummy_function
+      sum_vector%v1 = self%v1 + other%v1
+      sum_vector%v2 = self%v2 + other%v2
+      sum_vector%v3 = self%v3 + other%v3
+   end function vector_add_to
+
+   pure function vector_distance_to(self, other) result(distance)
+      class(t_3d_vector), intent(in) :: self
+      class(t_3d_vector), intent(in) :: other
+
+      real(kind=wp) :: distance
+
+      distance = sqrt((self%v1-other%v1)**2 + (self%v2-other%v2)**2 + (self%v3-other%v3)**2)
+   end function vector_distance_to
 
 end module m_vector
--- test/test_vector_lib.f90
+++ test/test_vector_lib.f90
@@ -1,6 +1,7 @@
 module test_vector_lib
    use testdrive, only: new_unittest, unittest_type, error_type, check, test_failed
-   use m_vector, only: dummy_function
+   use m_vector, only: t_3d_vector
+   use iso_fortran_env, only: wp => real64
 
    implicit none(type, external)
 
@@ -14,15 +15,44 @@ contains
       type(unittest_type), allocatable, intent(out) :: testsuite(:)
 
       testsuite = [ &
-                  new_unittest("dummy_function", test_dummy_function) &
+                  new_unittest("test_3d_vector_distance", test_3d_vector_distance), &
+                  new_unittest("test_3d_vector_add", test_3d_vector_add) &
                   ]
 
    end subroutine collect_tests
 
-   subroutine test_dummy_function(error)
+   subroutine test_3d_vector_distance(error)
       type(error_type), allocatable, intent(out) :: error
 
-      call check(error, dummy_function())
-   end subroutine test_dummy_function
+      type(t_3d_vector) :: vector1
+      type(t_3d_vector) :: vector2
+
+      ! sqrt( (4-1)**2 + (6-2)**2 + (8-3)**2 )
+      ! = sqrt( 9 + 16 + 25)
+      ! = sqrt(50)
+      vector1 = t_3d_vector(4.0_wp,6.0_wp,8.0_wp)
+      vector2 = t_3d_vector(1.0_wp,2.0_wp,3.0_wp)
+
+      call check(error, vector1%distance_to(vector2), sqrt(50.0_wp))
+   end subroutine test_3d_vector_distance
+
+   subroutine test_3d_vector_add(error)
+      type(error_type), allocatable, intent(out) :: error
+
+      type(t_3d_vector) :: vector1
+      type(t_3d_vector) :: vector2
+      type(t_3d_vector) :: expected
+      type(t_3d_vector) :: actual
+
+      vector1 = t_3d_vector(4.0_wp,6.0_wp,8.0_wp)
+      vector2 = t_3d_vector(1.0_wp,2.0_wp,3.0_wp)
+
+      expected = t_3d_vector(5.0_wp, 8.0_wp, 11.0_wp)
+      actual = vector1%add_to(vector2)
+
+      call check(error, expected%v1, actual%v1)
+      call check(error, expected%v2, actual%v2)
+      call check(error, expected%v3, actual%v3)
+   end subroutine test_3d_vector_add
 
 end module test_vector_lib
