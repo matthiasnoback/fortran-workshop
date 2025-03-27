1ad089e097df5c8a174d3c1743927b31e5fc42bd
noback <Matthias.Noback@deltares.nl>
2025-03-27 14:10:29 +0100

    Update logger code for workshop 2

--- src/logging_demo/logging/m_logging.f90
+++ src/logging_demo/logging/m_logging.f90
@@ -1,12 +1,11 @@
+
 module m_logging
    use m_command_line, only: has_cli_argument
 
    implicit none(type, external)
 
-   logical :: debug
-   logical :: quiet
-   logical :: should_log_to_file
-   integer :: log_file
+   private
+   public :: get_logger
 
    type, abstract :: t_abstract_logger
    contains
@@ -18,41 +17,114 @@ module m_logging
          import t_abstract_logger
          implicit none(type, external)
 
-         class(t_abstract_logger), intent(in) :: self
+         class(t_abstract_logger), intent(inout) :: self
          character(len=*), intent(in) :: message
       end subroutine log_function_interface
    end interface
 
-   type, extends(t_abstract_logger) :: t_do_everything_logger
+   type, extends(t_abstract_logger) :: t_file_logger
+      integer :: log_file_unit
    contains
-      procedure :: log => do_everytying_logger_log
+      procedure :: log => file_logger_log
+   end type t_file_logger
+
+   interface t_file_logger
+      procedure :: file_logger_constructor
+   end interface
+
+   type, extends(t_abstract_logger) :: t_stdout_logger
+   contains
+      procedure :: log => stdout_logger_log
+   end type t_stdout_logger
+
+   type, extends(t_abstract_logger) :: t_do_everything_logger
+      private
+
+      logical :: debug
+      logical :: quiet
+
+      class(t_file_logger), allocatable :: file_logger
+      class(t_stdout_logger), allocatable :: stdout_logger
+   contains
+      procedure :: log => do_everything_logger_log
    end type t_do_everything_logger
 
+   interface t_do_everything_logger
+      procedure :: do_everything_logger_constructor
+   end interface
+
+   class(t_abstract_logger), allocatable, target :: the_logger
 contains
-   subroutine do_everytying_logger_log(self, message)
-      class(t_do_everything_logger), intent(in) :: self
+   function do_everything_logger_constructor(debug, quiet) result(logger)
+      logical, intent(in) :: debug
+      logical, intent(in) :: quiet
+      type(t_do_everything_logger) :: logger
+
+      logger%debug = debug
+      logger%quiet = quiet
+      logger%file_logger = t_file_logger('debug.log')
+      logger%stdout_logger = t_stdout_logger()
+   end function do_everything_logger_constructor
+
+   function file_logger_constructor(log_file_path) result(logger)
+      character(len=*), intent(in) :: log_file_path
+      type(t_file_logger) :: logger
+
+      integer :: open_status
+      integer :: log_file_unit
+
+      open (file=log_file_path, newunit=log_file_unit, status='unknown', &
+            position='append', action='write', iostat=open_status)
+
+      if (open_status /= 0) then
+         error stop "Could not open log file for writing"
+      end if
+
+      logger%log_file_unit = log_file_unit
+   end function file_logger_constructor
+
+   subroutine file_logger_log(self, message)
+      class(t_file_logger), intent(inout) :: self
       character(len=*), intent(in) :: message
 
-      if (debug) then
-         if (should_log_to_file) then
-            write (log_file, fmt=*) current_time()//' '//message
-         end if
+      write (self%log_file_unit, fmt=*) current_time()//' '//message
+   end subroutine file_logger_log
 
-         if (.not. quiet) then
-            print *, current_time()//' '//message
+   subroutine stdout_logger_log(self, message)
+      class(t_stdout_logger), intent(inout) :: self
+      character(len=*), intent(in) :: message
+
+      print *, current_time()//' '//message
+   end subroutine stdout_logger_log
+
+   subroutine do_everything_logger_log(self, message)
+      class(t_do_everything_logger), intent(inout) :: self
+      character(len=*), intent(in) :: message
+
+      if (self%debug) then
+         call self%file_logger%log(message)
+
+         if (.not. self%quiet) then
+            call self%stdout_logger%log(message)
          end if
       end if
-   end subroutine do_everytying_logger_log
+   end subroutine do_everything_logger_log
 
-   subroutine init_logging()
-      integer :: open_status
+   function get_logger() result(logger)
+      class(t_abstract_logger), pointer :: logger
 
-      debug = .not. has_cli_argument('--no-debug')
-      quiet = has_cli_argument('--quiet')
-      open (file='debug.log', newunit=log_file, status='unknown', &
-            position='append', action='write', iostat=open_status)
-      should_log_to_file = open_status == 0
-   end subroutine init_logging
+      logical :: debug
+      logical :: quiet
+
+      if (.not. allocated(the_logger)) then
+         debug = .not. has_cli_argument('--no-debug')
+         quiet = has_cli_argument('--quiet')
+
+         the_logger = t_do_everything_logger(debug, quiet)
+      end if
+
+      logger => the_logger
+   end function get_logger
 
    function current_time() result(iso_time)
       character(len=8) :: date
--- src/logging_demo/main.f90
+++ src/logging_demo/main.f90
@@ -1,16 +1,9 @@
 program example
-   use m_logging, only: init_logging, t_abstract_logger, t_do_everything_logger
+   use m_logging, only: get_logger
 
    implicit none(type, external)
 
-   ! Depend on the abstract logger
-   class(t_abstract_logger), allocatable :: logger
+   call get_logger()%log('A message')
 
-   call init_logging()
-
-   ! Instantiate the concrete logger
-   logger = t_do_everything_logger()
-
-   call logger%log('[config] Loaded')
-
+   call get_logger()%log('Another message')
 end program example
