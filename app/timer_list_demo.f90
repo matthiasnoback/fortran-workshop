fdc653dd7d7f8af932454d4bbce8d7e6170aaefe
noback <Matthias.Noback@deltares.nl>
2025-04-15 13:27:11 +0200

    Assignment: Set the end time on a timer

--- app/timer_list_demo.f90
+++ app/timer_list_demo.f90
@@ -7,12 +7,18 @@ program example
    type(timer_list_t) :: timer_list
    type(timer_t) :: timer1
    type(timer_t) :: timer2
+   type(timer_t), pointer :: found_timer
 
    timer1 = timer_t('Column-first')
    timer2 = timer_t('Row-first')
 
    call timer_list%add(timer1)
    call timer_list%add(timer2)
+
+   found_timer => timer_list%get('Column-first')
+   if (associated(found_timer)) then
+      call found_timer%stop()
+   end if
 
    call timer_list%print_all()
 
--- src/stopwatch/timer_list.f90
+++ src/stopwatch/timer_list.f90
@@ -20,13 +20,25 @@ contains
       class(timer_list_t), intent(inout) :: self
       type(timer_t), intent(in) :: timer
 
-      ! TODO add timer instance to self%timers array
+      if (allocated(self%timers)) then
+         self%timers = [self%timers, timer]
+      else
+         self%timers = [timer]
+      endif
    end subroutine timer_list_add
 
    subroutine timer_list_print_all(self)
       class(timer_list_t), intent(inout) :: self
 
-      ! TODO print all timers
+      integer :: i
+
+      if (.not. allocated(self%timers)) then
+         return
+      end if
+
+      do i = 1, size(self%timers)
+         call self%timers(i)%print()
+      end do
    end subroutine timer_list_print_all
 
    function timer_list_get(self, label) result(timer)
@@ -34,7 +46,16 @@ contains
       character(len=*), intent(in) :: label
       type(timer_t), pointer :: timer
 
-      ! TODO loop over self%timers and return the timer that has the given label
+      integer :: i
+
+      if (allocated(self%timers)) then
+         do i = 1, size(self%timers)
+            if (self%timers(i)%label == label) then
+               timer => self%timers(i)
+               return
+            end if
+         end do
+      end if
 
       timer => null()
    end function timer_list_get
