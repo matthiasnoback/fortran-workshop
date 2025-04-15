4e164732919dcdeac6086e9f69c6c152f7ec0cb6
noback <Matthias.Noback@deltares.nl>
2025-04-15 12:14:35 +0200

    Add empty modules for assignments

---
+++ src/logging/aggregation.f90
@@ -0,0 +1,7 @@
+module logging_aggregation
+   use logging_abstract, only: abstract_logger_t
+
+   implicit none(type, external)
+
+   private
+end module logging_aggregation
---
+++ src/logging/timestamp.f90
@@ -0,0 +1,8 @@
+module logging_timestamp
+   use common_time, only: current_time
+   use logging_abstract, only: abstract_logger_t
+
+   implicit none(type, external)
+
+   private
+end module logging_timestamp
