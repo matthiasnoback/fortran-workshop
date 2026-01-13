b5bed813a0c1ee48de96ffd9b3df51068820a542
noback <Matthias.Noback@deltares.nl>
2026-01-13 15:00:45 +0100

    inFile and outFile as arguments

--- app/rainfall_runoff_demo.f90
+++ app/rainfall_runoff_demo.f90
@@ -1,14 +1,14 @@
 program rainfall_runoff_demo
    use iso_fortran_env, only: real64
    use hydrology_rainfall_runoff, only: run, &
-                                        inFile, &
-                                        outFile, &
                                         parse_args
 
    implicit none(type, external)
 
+   character(len=256), intent(out) :: inFile, outFile
+
    call parse_args(inFile, outFile)
 
-   call run()
+   call run(inFile, outFile)
 
 end program rainfall_runoff_demo
--- src/hydrology/rainfall_runoff.f90
+++ src/hydrology/rainfall_runoff.f90
@@ -26,8 +26,6 @@ module hydrology_rainfall_runoff
 
    private
    public :: run
-   public :: inFile
-   public :: outFile
    public :: parse_args
 
    ! --------------------------
@@ -51,7 +49,6 @@ module hydrology_rainfall_runoff
    ! S: soil water storage (mm). Start at 60% of capacity as a neutral initial condition.
    real(real64) :: S = 0.6_real64*Smax
 
-   character(len=256) :: inFile, outFile ! file paths
    integer :: uin, uout, ios ! unit numbers + I/O status
    character(len=512) :: line ! line buffer and date string
    character(len=:), allocatable :: date
@@ -62,7 +59,8 @@ module hydrology_rainfall_runoff
 
 contains
 
-   subroutine run()
+   subroutine run(inFile, outFile)
+      character(len=*), intent(in) :: inFile, outFile
 
       open (newunit=uin, file=trim(inFile), status='old', action='read', iostat=ios)
       if (ios /= 0) stop 'Cannot open input CSV.'
